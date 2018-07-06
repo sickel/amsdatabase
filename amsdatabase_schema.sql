df5a78e0580dd672fcc783f38c8e5d782900413a
Morten Sickel <morten@sickel.net>
2018-07-06 12:37:22 +0200

    default current_user on addby, inserted some data

--- amsdatabase_schema.sql
+++ amsdatabase_schema.sql
@@ -3,25 +3,30 @@ create table system(
     name varchar(255),
     description text,
     addtime datetime default current_timestamp,
-    addby varchar(100)
+    addby varchar(100) default current_user
 );
+
+alter table system add constraint UK_systemname unique (name);
 
 create table project(
     id integer primary key not null IDENTITY(1,1),
     name varchar(255),
     training bit not null default 1,
     addtime datetime default current_timestamp,
-    addby varchar(100)
+    addby varchar(100) default current_user
 );
+
+alter table project add constraint UK_projectname unique  (name);
 
 create table survey(
     id integer primary key not null IDENTITY(1,1),
+    name varchar(100) not null,
     startup datetime not null,
     systemid integer not null references system(id),
     projectid integer references project(id),  
     description text,
     addtime datetime default current_timestamp,
-    addby varchar(100)
+    addby varchar(100) default current_user
 );
 
 
@@ -31,13 +36,15 @@ create table datafile(
     md5 varchar(100),
     surveyid integer not null references survey(id),
     addtime datetime default current_timestamp,
-    addby varchar(100)
+    addby varchar(100) default current_user
     );
 
+alter table datafile add constraint UK_datafilename unique  (filename);
+    
 create table measurement(
     id integer primary key not null IDENTITY(1,1),
     addtime datetime default current_timestamp,
-    addby varchar(100),
+    addby varchar(100) default current_user,
     datafileid integer not null references datafile(id),
     SeqNum integer not null,
     systemtime datetime not null,
@@ -61,17 +68,18 @@ create table measurement(
 create table detector(
     id integer primary key not null IDENTITY(1,1),
     addtime datetime default current_timestamp,
-    addby varchar(100),
+    addby varchar(100) default current_user,
     systemid integer not null references system(id),
     name varchar(50),
     type varchar(50),
     volumeliter integer    
 ); 
+alter table detector add constraint UK_detectorsystem (systemid,name)
  
 create table detectormeasure(
     id integer primary key not null IDENTITY(1,1),
     addtime datetime default current_timestamp,
-    addby varchar(100),
+    addby varchar(100) default current_user,
     name varchar(20),
     ndet integer not null,
     dose numeric,
@@ -100,3 +108,18 @@ create table updatefile(
     md5 varchar(100)
 )
 
+
+
+insert into system(name,description) values('330-1','4x4 liter RS-500');
+insert into system(name,description) values('330-2','4x4 liter RS-500');
+insert into system(name,description) values('NGU','5x4 liter RS-500');
+insert into system(name,description) values('NRPA-25l','5x4 liter RS-500');
+insert into system(name,description) values('NRPA-Østerås','RS-701');
+insert into system(name,description) values('NRPA-Svanhovd','RS-701');
+
+insert into detector(systemid,name,type,volumeliter) (select id,'Total','NaI',16 from system where name='330-1');
+insert into detector(systemid,name,type,volumeliter) (select id,'Total','NaI',8 from system where name='NRPA-Østerås');
+insert into detector(systemid,name,type,volumeliter) (select id,'Left','NaI',4 from system where name='NRPA-Østerås');
+insert into detector(systemid,name,type,volumeliter) (select id,'Right','NaI',4 from system where name='NRPA-Østerås');
+insert into detector(systemid,name,type,volumeliter) (select id,'3"','NaI',1 from system where name='NRPA-Østerås');
+insert into detector(systemid,name,type,volumeliter) (select id,'Nøytron','Nøytron',0 from system where name='NRPA-Østerås');
